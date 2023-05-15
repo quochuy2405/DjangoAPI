01b5841b738b9f591224af497d3157d80fda1905
quochuy2405 <huy01682312920@gmail.com>
2023-05-15 10:55:11 +0700

    update

--- src/connection/connect.py
+++ src/connection/connect.py
@@ -3,7 +3,7 @@ def connect():
     try:
       
         # establish a connection to the database
-        connection = cx_Oracle.connect('sys/123456@//localhost:1521/orcl',mode=cx_Oracle.SYSDBA)
+        connection = cx_Oracle.connect('dsadd/123456@//localhost:1521/orcl',mode=cx_Oracle.SYSDBA)
 
         # print a success message
         print('Successfully connected to Oracle database')
--- src/products/views.py
+++ src/products/views.py
@@ -4,8 +4,16 @@ from connection import connect as conn
 # Create your views here.
 def getAllProducts(res):
     con = conn.connect()
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM IS_PRODUCT')
+    rows = cursor.fetchall()
+     # close the cursor and connection
+    cursor.close()
     con.close()
+    print(rows)
+    # create a response dictionary containing the rows
     response_data = {
-        "conn":"ok"
-        }
-    return JsonResponse(response_data, status=201)
+        'rows': rows,
+        'message': 'Retrieved all rows from IS_PRODUCT table successfully.'
+    }
+    return JsonResponse(response_data, status=200)
