c5be7d7e440915878f4c7ee29cb520180deceb37
quochuy2405 <huy01682312920@gmail.com>
2023-05-15 12:40:19 +0700

    update connect db

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from . import views
-from products import views as pr
+from products import controllers as pr
 urlpatterns = [
-    path('get_all/', views.getData),
     path('get/', pr.getAllProducts),
 ]
--- src/api/views.py
+++ src/api/views.py
@@ -1,11 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from base.models import Item
-from .serializers import ItemSerializers
 
 @api_view(['GET'])
 
 def getData(request):
-    item= Item.objects.all()
-    serialize = ItemSerializers(item,many=True)
-    return Response(serialize.data)
+  
+    return Response({})
