f48c5665c2bc48f81258a14fe2b168c21a4e58ea
quochuy2405 <huy01682312920@gmail.com>
2023-05-15 12:53:56 +0700

    update connect db

--- src/products/controllers.py
+++ src/products/controllers.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.db import connection  
+from django.db import connection
 # Create your views here.
 def getAllProducts(res):
     
@@ -8,12 +8,17 @@ def getAllProducts(res):
     cursor.execute('SELECT * FROM IS_PRODUCT')
     rows = cursor.fetchall()
      # close the cursor and connection
+     # get the column names
+    columns = [desc[0] for desc in cursor.description]
+
+    # convert rows into a list of dictionaries
+    data = [dict(zip(columns, row)) for row in rows]
     cursor.close()
     connection.close()
     print(rows)
     # create a response dictionary containing the rows
     response_data = {
-        'rows': rows,
+        'rows': data,
         'message': 'Retrieved all rows from IS_PRODUCT table successfully.'
     }
     return JsonResponse(response_data, status=200)
