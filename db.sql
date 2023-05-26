5f187319f0e2a34258eb2f142af532b617a4e03a
quochuy2405 <huy01682312920@gmail.com>
2023-05-26 09:50:51 +0700

    update db

--- db.sql
+++ db.sql
@@ -56,9 +56,10 @@ CREATE TABLE orders (
 CREATE TABLE product_order (
     product_id VARCHAR2(50),
     order_id NUMBER,
+    quantity NUMBER,
     CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES products(id),
     CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES orders(id),
-    PRIMARY KEY (product_id, order_id)
+    PRIMARY KEY (product_id, order_id,quantity)
 );
 
 CREATE TABLE wallet (
@@ -67,6 +68,54 @@ CREATE TABLE wallet (
     balance NUMBER(10, 2) NOT NULL,
     CONSTRAINT fk_user_wallet_id FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+-- KHI MUA NHIỀU HƠN SỐ LƯỢNG TỒN KHO
+CREATE OR REPLACE TRIGGER check_product_quantity
+BEFORE INSERT ON product_order
+FOR EACH ROW
+DECLARE
+    v_product_quantity NUMBER;
+    v_over_quantity NUMBER;
+BEGIN
+    -- Lấy số lượng hiện có của sản phẩm
+    SELECT quantity INTO v_product_quantity
+    FROM products
+    WHERE id = :NEW.product_id;
+
+    -- Kiểm tra nếu số lượng mua lớn hơn số lượng hiện có
+    IF :NEW.quantity > v_product_quantity THEN
+        v_over_quantity:= :NEW.quantity- v_product_quantity;
+        -- Gây ra một lỗi và ngăn chặn việc thêm bản ghi vào bảng product_order
+        RAISE_APPLICATION_ERROR(-20001, 'Số lượng mua vượt quá '|| v_over_quantity ||' sản phẩm.');
+    END IF;
+
+      -- Giảm số lượng tồn kho sau khi mua hàng
+    v_product_quantity := v_product_quantity - :NEW.quantity ;
+
+    -- Cập nhật số lượng tồn kho mới vào bảng products
+    UPDATE products
+    SET quantity = v_product_quantity
+    WHERE id = :NEW.product_id;
+END;
+/
+
+-- TEST TRIGGER
+-- Thêm dữ liệu vào bảng products
+INSERT INTO products (id, name, category_id, quantity)
+VALUES ('P1', 'Product 1', 'C1', 5);
+
+-- Thêm dữ liệu vào bảng orders
+INSERT INTO orders (id, user_id)
+VALUES (1, 1);
+
+-- Thêm dữ liệu vào bảng product_order với số lượng mua hợp lệ
+INSERT INTO product_order (product_id, order_id, quantity)
+VALUES ('P1', 1, 3);
+
+-- Thêm dữ liệu vào bảng product_order với số lượng mua vượt quá số lượng hiện có
+INSERT INTO product_order (product_id, order_id, quantity)
+VALUES ('P1', 1, 7);
+
 
 INSERT INTO categories (id, name) VALUES
 ('1', 'Thời trang nam');
@@ -110,24 +159,24 @@ INSERT INTO orders (id, addressNumber, ward, district, province, checkoutId, ema
 INSERT INTO orders (id, addressNumber, ward, district, province, checkoutId, email, name, user_id) VALUES
 (5, '654 JKL Street', 'Ward 5', 'District E', 'Province V', 'CHK654', 'hoangvane@example.com', 'Hoang Van E', 5);
 
-INSERT INTO product_order (product_id, order_id) VALUES
-('P1', 1);
-INSERT INTO product_order (product_id, order_id) VALUES
-('P2', 1);
-INSERT INTO product_order (product_id, order_id) VALUES
-('P3', 2);
-INSERT INTO product_order (product_id, order_id) VALUES
-('P4', 2);
-INSERT INTO product_order (product_id, order_id) VALUES
-('P5', 3);
-INSERT INTO product_order (product_id, order_id) VALUES
-('P6', 4);
-INSERT INTO product_order (product_id, order_id) VALUES
-('P1', 5);
-INSERT INTO product_order (product_id, order_id) VALUES
-('P3', 5);
-INSERT INTO product_order (product_id, order_id) VALUES
-('P5', 5);
+INSERT INTO product_order (product_id, order_id,quantity) VALUES
+('P1', 1,1);
+INSERT INTO product_order (product_id, order_id,quantity) VALUES
+('P2', 1,1);
+INSERT INTO product_order (product_id, order_id,quantity) VALUES
+('P3', 2,1);
+INSERT INTO product_order (product_id, order_id,quantity) VALUES
+('P4', 2,1);
+INSERT INTO product_order (product_id, order_id,quantity) VALUES
+('P5', 3,1);
+INSERT INTO product_order (product_id, order_id,quantity) VALUES
+('P6', 4,1);
+INSERT INTO product_order (product_id, order_id,quantity) VALUES
+('P1', 5,1);
+INSERT INTO product_order (product_id, order_id,quantity) VALUES
+('P3', 5,1);
+INSERT INTO product_order (product_id, order_id,quantity) VALUES
+('P5', 5,1);
 
 INSERT INTO wallet (id, user_id, balance) VALUES
 (1, 1, 1000.00);
@@ -140,22 +189,3 @@ INSERT INTO wallet (id, user_id, balance) VALUES
 INSERT INTO wallet (id, user_id, balance) VALUES
 (5, 5, 50.50);
 
-
-CREATE OR REPLACE TRIGGER check_product_quantity
-BEFORE INSERT ON product_order
-FOR EACH ROW
-DECLARE
-    v_product_quantity NUMBER;
-BEGIN
-    -- Lấy số lượng hiện có của sản phẩm
-    SELECT quantity INTO v_product_quantity
-    FROM products
-    WHERE id = :NEW.product_id;
-
-    -- Kiểm tra nếu số lượng mua lớn hơn số lượng hiện có
-    IF :NEW.quantity > v_product_quantity THEN
-        -- Gây ra một lỗi và ngăn chặn việc thêm bản ghi vào bảng product_order
-        RAISE_APPLICATION_ERROR(-20001, 'Số lượng mua vượt quá số lượng hiện có của sản phẩm.');
-    END IF;
-END;
-/
